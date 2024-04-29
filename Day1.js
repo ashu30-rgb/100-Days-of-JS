d039d69cef56dad5a3f2819752503ab668184576
Ashutosh <ashutosh.verma@sysfore.com>
2024-04-29 18:37:04 +0530

    Day 1

---
+++ Day1.js
@@ -0,0 +1,11 @@
+const findLongestWord = (str) =>{
+    if(str.trim().length===0){
+        return false
+    }
+
+    words = str.split(" ")
+    words = words.sort((a,b)=>a.length - b.length)
+    return words.at(-1)
+}
+
+console.log(findLongestWord("Hello howwwwwww are you "))
