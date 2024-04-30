6581739735c666d9598335ef37a47c34bb55efe5
Ashutosh <ashutosh728173@gmail.com>
2024-04-30 11:40:49 +0530

    Day 3

--- Day3.js
+++ Day3.js
@@ -1,17 +1,17 @@
 const countChar = (str, char) =>{
-//     let count = 0;
-// for(let i=0; i<str.length; i++){
-//     if(str[i].toLowerCase()==char.toLowerCase()){
-//         count += 1
-//     }
-// }
-count = str.split("").reduce((accum, curChar)=>{
-    if(curChar.toLowerCase()==char.toLowerCase()){
-        accum++
+    //     let count = 0;
+    // for(let i=0; i<str.length; i++){
+    //     if(str[i].toLowerCase()==char.toLowerCase()){
+    //         count += 1
+    //     }
+    // }
+    count = str.split("").reduce((accum, curChar)=>{
+        if(curChar.toLowerCase()==char.toLowerCase()){
+            accum++
+        }
+        return accum
+    },0)
+    return count
     }
-    return accum
-},0)
-return count
-}
-
-console.log(countChar("MissIssippi", "I"))
+    
+    console.log(countChar("MissIssippi", "I"))
