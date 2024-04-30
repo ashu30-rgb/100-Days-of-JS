3c153274e368f146ae7529990cb6383c4fe445e8
Ashutosh <ashutosh728173@gmail.com>
2024-04-30 11:40:21 +0530

    Day 2

--- Day2.js
+++ Day2.js
@@ -1,12 +1,13 @@
 const generateHash = (str) => {
-  if (str.trim().length == 0 || str.trim().length > 280) {
-    return false;
-  }
-  let res = "";
-  str.trim().split(" ").map((item) => {
-      res = res + item.charAt(0).toUpperCase() + item.slice(1);
-    });
-  return "#" + res;
-};
-
-console.log(generateHash("Hello how are you"));
+    if (str.trim().length == 0 || str.trim().length > 280) {
+      return false;
+    }
+    let res = "";
+    str.trim().split(" ").map((item) => {
+        res = res + item.charAt(0).toUpperCase() + item.slice(1);
+      });
+    return "#" + res;
+  };
+  
+  console.log(generateHash("Hello how are you"));
+  
