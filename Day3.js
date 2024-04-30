530ac4d1d130c01ea4489ea17c7e7f91ae646f4d
Ashutosh <ashutosh.verma@sysfore.com>
2024-04-30 11:12:32 +0530

    Day 3

---
+++ Day3.js
@@ -0,0 +1,17 @@
+const countChar = (str, char) =>{
+//     let count = 0;
+// for(let i=0; i<str.length; i++){
+//     if(str[i].toLowerCase()==char.toLowerCase()){
+//         count += 1
+//     }
+// }
+count = str.split("").reduce((accum, curChar)=>{
+    if(curChar.toLowerCase()==char.toLowerCase()){
+        accum++
+    }
+    return accum
+},0)
+return count
+}
+
+console.log(countChar("MissIssippi", "I"))
