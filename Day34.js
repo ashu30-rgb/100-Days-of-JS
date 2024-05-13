32a387c2304aea81558add1964055ce8947ac944
Ashutosh <ashutosh728173@gmail.com>
2024-05-13 13:09:01 +0530

    Day 34

---
+++ Day34.js
@@ -0,0 +1,7 @@
+const removeDuplicates = (arr) =>{
+    const newArr =[]
+    return [...new Set(arr)];
+};
+
+console.log(removeDuplicates([1,2,3,3,4,4,5]));
+console.log(removeDuplicates(["a", "b", "c", "b", "a"]));
