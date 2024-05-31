857ec78a3c01392087a69385296c57d604b05fca
Ashutosh <ashutosh728173@gmail.com>
2024-05-31 10:26:31 +0530

    Day 40 method 2

--- Day40.js
+++ Day40.js
@@ -1,13 +1,19 @@
 const generateBarChart = (arr) => {
-    const newArr = arr.map((curElem, index)=>{
-        let star = "";
-        let number = 0;
-        while(number<curElem){
-            star += "*";
-            number ++
-        }
-        console.log(`${index+1}:${star}`)
+    // const newArr = arr.map((curElem, index)=>{
+    //     let star = "";
+    //     let number = 0;
+    //     while(number<curElem){
+    //         star += "*";
+    //         number ++
+    //     }
+    //     return `${index+1}:${star}`
+    // })
+    // return newArr.join("\n")
+
+    return arr.map((curElem, index)=>{
+        return `${index+1}:${"*".repeat(curElem)}`
     })
+    .join("\n")
 };
 
 console.log(generateBarChart([5,3,9,2]))
